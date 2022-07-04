342a5485857fc78090db423ef49acb49cd334e86
hank-chiu <hank.chiu@gofreight.co>
2022-07-04 11:46:40 +0800

    init

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
-const SVGIcons2SVGFontStream = require('svgicons2svgfont');
+const FontStream = require('svgicons2svgfont');
 const svg2ttf = require('svg2ttf');
 const fs = require('fs');
-const fontStream = new SVGIcons2SVGFontStream({
+
+const fontStream = new FontStream({
   fontName: 'gofreight',
   fontHeight: 1000,
   normalize: true,
 });
 
+const fontSvg = 'icon.svg';
+const fontTtf = 'icon.ttf';
+const svgSource = 'icons';
+
 // Setting the font destination
 fontStream
-  .pipe(fs.createWriteStream('icon.svg'))
+  .pipe(fs.createWriteStream(fontSvg))
   .on('finish', function () {
-    const ttf = svg2ttf(fs.readFileSync('icon.svg', 'utf8', {}));
-    fs.writeFileSync('icon.ttf', Buffer.from(ttf.buffer));
+    const ttf = svg2ttf(fs.readFileSync(fontSvg, 'utf8', {}));
+    fs.writeFileSync(fontTtf, Buffer.from(ttf.buffer));
     console.log('Font successfully created!');
   })
   .on('error', function (err) {
     console.log(err);
   });
 
+const sources = fs.readdirSync(svgSource);
+
 // Writing glyphs
-const glyph1 = fs.createReadStream('icons/copy_to.svg');
-glyph1.metadata = {
-  unicode: ['copy_to'],
-  name: 'copy_to',
-};
-fontStream.write(glyph1);
+sources.forEach((svgFile) => {
+  const name = svgFile.split('.')[0];
+  const glyph = fs.createReadStream(`${svgSource}/${svgFile}`);
+  glyph.metadata = {
+    unicode: [name],
+    name,
+  };
+  fontStream.write(glyph);
+  console.log('write glyph:', svgFile)
+});
 
 
 // Do not forget to end the stream
