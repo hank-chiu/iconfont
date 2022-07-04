1cc22d5757fd08d237a602f640f132d7b31b712d
hank-chiu <hank.chiu@gofreight.co>
2022-07-04 11:16:27 +0800

    1 binaries uploaded

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-// run `node index.js` in the terminal
+const SVGIcons2SVGFontStream = require('svgicons2svgfont');
+const fs = require('fs');
+const fontStream = new SVGIcons2SVGFontStream({
+  fontName: 'hello',
+});
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// Setting the font destination
+fontStream
+  .pipe(fs.createWriteStream('icon.svg'))
+  .on('finish', function () {
+    console.log('Font successfully created!');
+  })
+  .on('error', function (err) {
+    console.log(err);
+  });
+
+// Writing glyphs
+const glyph1 = fs.createReadStream('icons/copy-to.svg');
+glyph1.metadata = {
+  unicode: ['copy-to'],
+  name: 'copy-to',
+};
+fontStream.write(glyph1);
+
+
+// Do not forget to end the stream
+fontStream.end();
